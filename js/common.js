58872174cfc2c36e1ab2443400b08af6db9336f8
leepuxin <brainupresearch@outlook.com>
2023-03-02 12:11:25 +0800

    增加标题 可折叠/展开 功能

--- js/common.js
+++ js/common.js
@@ -1,14 +1,25 @@
-
 // 单击标题内容 => 折叠 or 展开
 function bindFoldOrExpendEvent(_this){
     let spanEle = $(_this).next();
+    let arrow_span = $(_this).children()[0];
 
-    if( spanEle.css('display') === 'none' ){
+    let isFold = JSON.parse(spanEle.attr('isfold'));
+
+    // console.log(isFold)
+
+    if (isFold){
+        // 当前为折叠; 设置为展开
+        spanEle.attr('isfold', false)
+        arrow_span.innerHTML = ''; // 不可见字符; 显示为 下箭头
+
+        // 显示标题包含的内容
         $(spanEle).show();
+    }else {
+        // 当前为展开; 设置为折叠
+        spanEle.attr('isfold', true)
+        arrow_span.innerHTML = ''; // 不可见字符; 显示为 右箭头
 
-    }else if(spanEle.css('display') === 'inline'){
+        // 隐藏标题包含的内容
         $(spanEle).hide();
-
     }
-
 }
